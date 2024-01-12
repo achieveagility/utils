559f4ffef39d83906787faf4e957d64d0d4c25f1
achieveagility <72562041+achieveagility@users.noreply.github.com>
2024-01-12 11:45:48 +0000

    fix: update vite (#10)

--- packages/deepmerge/vite.config.ts
+++ packages/deepmerge/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vitest/config";
-// import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    // viteTsconfigPaths(),
-  ],
+  plugins: [],
   test: {
     globals: true,
   },
