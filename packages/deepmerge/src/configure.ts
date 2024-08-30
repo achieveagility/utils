aea549ddb59c882a135b1bd42ca295e727219a16
achieveagility <achieveagility.contact@gmail.com>
2024-08-30 19:06:37 +0100

    wip: update

--- packages/deepmerge/src/configure.ts
+++ packages/deepmerge/src/configure.ts
@@ -4,15 +4,15 @@ function isObject(value?: unknown): value is Record<string, unknown> {
   return Object.prototype.toString.call(value) === '[object Object]';
 }
 
-function isValid(val: unknown) {
-  return val === undefined || val === null || isObject(val);
+function isValid(value: unknown) {
+  return value === undefined || value === null || isObject(value);
 }
 
-export function configure(conf?: object) {
+export function configure(config?: object) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return <T = unknown>(...params: any): T => {
+  return <T = unknown>(...parameters: any): T => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if (params.some((p: any) => !isValid(p))) {
+    if (parameters.some((p: any) => !isValid(p))) {
       throw new Error(
         'All merge parameters are expected to be objects, null, or undefined.',
       );
@@ -20,8 +20,8 @@ export function configure(conf?: object) {
 
     return deepmergelib.all(
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      params.map((p: any) => p || {}),
-      conf,
+      parameters.map((p: any) => p || {}),
+      config,
     ) as T;
   };
 }
